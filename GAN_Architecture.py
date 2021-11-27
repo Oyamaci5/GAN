2188b8cf7802501b1b573ef2cee6f0dac45f36f8
Oğuzhan Yamacı <77755781+Oyamaci5@users.noreply.github.com>
2021-11-27 20:49:01 +0300

    Add files via upload

---
+++ GAN_Architecture.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+import torch
+import torch.nn as nn
+
+
+ngf = 64
+ndf = 64
+nz = 100
+
+class Generator(nn.Module):
+    def __init__(self, ngpu):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        
+        self.conv1 = nn.ConvTranspose3d(nz, ngf*16, 4, 1, 0, bias= False )
+        
+        self.main_conv=nn.Sequential(
+            
+            nn.ConvTranspose3d(ngf*16, ngf*8, 4, 2, 1, bias = False),
+            nn.BatchNorm3d(ngf*8),
+            nn.ReLU(True),
+            
+            nn.ConvTranspose3d(ngf*8, ngf*4, 4, 2, 1, bias = False),
+            nn.BatchNorm3d(ngf*4),
+            nn.ReLU(True),
+            
+            nn.ConvTranspose3d(ngf*4, ngf*2, 4, 2, 1, bias = False),
+            nn.BatchNorm3d(ngf*2),
+            nn.ReLU(True),
+             
+            nn.ConvTranspose3d(ngf*2, ngf, 4, 2, 1, bias = False),
+            nn.BatchNorm3d(ngf),
+            nn.ReLU(True),
+            
+            nn.ConvTranspose3d(ngf, 1, 3, 1, 2, bias=False),
+            nn.ReLU()
+            
+            )
+    def forward(self, noise, energy):
+        input = self.conv1(noise * energy)
+        return self.main_conv(input)
+    
+
+class Discriminator(nn.Module):
+    def __init__(self,ngpu):
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        
+        self.main_conv = nn.Sequential(
+                nn.Conv3d(1, ndf, 3, 2, 3, bias = False),
+                nn.LeakyReLU(0.2, inplace=True),
+                
+                nn.Conv3d(ndf, ndf*2, 3, 2, 1, bias= False),
+                nn.BatchNorm3d(ndf*2),
+                nn.LeakyReLU(0.2,inplace=True),
+                
+                nn.Conv3d(ndf*2, ndf*4, 3, 2, 1, bias= False),
+                nn.BatchNorm3d(ndf*4),
+                nn.LeakyReLU(0.2, inplace=True),
+                
+                nn.Conv3d(ndf*4, ndf*8, 3, 2, 1, bias= False),
+                nn.BatchNorm3d(ndf*8),
+                nn.LeakyReLU(0.2, inplace=True),
+                
+                nn.Conv3d(ndf*8, ndf*16, 3, 2, 1, bias= False)
+                
+            )
+        self.fc = nn.Sequential(
+                nn.Linear(ndf*8 +1,ndf*4),
+                nn.LeakyReLU(0.2, inplace=True),
+                
+                nn.Linear(ndf*4,1),
+                nn.Sigmoid()
+                )
+    def forward(self, ray ,energy):
+        conv_out = self.main_conv(ray)
+        fc_input = torch.cat((conv_out, energy), 1).view(-1,ndf*4 +1)
+        return self.fc(fc_input)
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.2)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+    elif classname.find('Linear')!= -1:
+        m.weight.data.normal_(0.0, 0.02)
+        m.bias.data.fill_(0)
---
+++ GammaknifeProje.py
@@ -0,0 +1,146 @@
+from __future__ import print_function
+#%matplotlib inline
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
+import HDF5Dataset as H
+import GAN_Architecture as GAN
+import sys
+ngpu = 1
+#Cuda check
+cuda = True if torch.cuda.is_available() else False
+FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+#Workers
+workers = 0
+#Parameters
+batch_size = 256
+feature_size = 90
+nz = 100
+#Number of channels 
+nc = 4
+#Epochs
+num_epochs = 10
+#Learning Rate
+lr= 0.0002
+#Beta1 hyperparam for Adam optimizers
+beta1 = 0.5
+#PATHS
+PATH_save = 'Save_model/conv3d_corr_new2.pth'
+PATH_chechpoint = 'Save_model/new2_1.pth'
+#Dataset
+#Gammaknife is the group name
+datapath = "Traindata.h5"
+dataset = H.HDF5Dataset(datapath, 'Gammaknife')
+#Create the dataLoader
+dataloader = torch.utils.data.DataLoader(dataset, batch_size = batch_size, 
+                                         shuffle=True, num_workers= workers)
+#Decide which device (GPU)
+device = torch.device("cuda:0" if(torch.cuda.is_available() and ngpu>0) else "cpu")
+
+
+#Create the generator
+NetG = GAN.Generator(ngpu).to(device)
+if(device.type == 'cuda') and (ngpu > 1):
+    NetG = nn.DataParallel(NetG, list(range(ngpu)))
+
+
+NetG.apply(GAN.weights_init)
+#Print the Generator model
+print(NetG)
+NetD = GAN.Discriminator(ngpu).to(device)
+if(device.type =='cuda') and (ngpu > 1):
+    NetD = nn.DataParallel(NetD, list(range(ngpu)))
+
+NetD.apply(GAN.weights_init)
+#Print the Discriminator model
+print(NetD)
+
+#Initialize BCELoss(Binary Cross Entropy Loss) function
+criterion = nn.BCELoss()
+
+fixed_noise = torch.randn(128,nz, 1, 1, device = device)
+real_label = 1.
+fake_label = 0.
+
+#Setup Adam optimizers for both G AND D
+optimizerD = optim.Adam(NetD.parameters(), lr=lr, betas=(beta1,0.999))
+optimizerG = optim.Adam(NetG.parameters(), lr = lr ,betas=(beta1, 0.999))
+
+
+#Training
+feature_list = []
+G_losses = []
+D_losses = []
+iters = 0
+
+print("Starting Training Loop")
+
+for epoch in range(num_epochs):
+    for i, data in enumerate(dataloader, 0):
+        #Train with all-real batch
+        NetD.zero_grad()
+        
+        real_cpu = data[0].to(device)
+        b_size = real_cpu.size(0)
+        label = torch.full((b_size,), real_label, dtype= torch.float,
+                           device = device)
+        
+        output = NetD(real_cpu).view(-1)
+        errD_real = criterion(output, label)
+        #Calculate gradients
+        errD_real.backward()
+        D_x = output.mean().item()
+        
+        #Generate batch of latent vectors
+        noise = torch.randn(b_size, nz, 1, 1, device =device)
+        #Generate fake image batch with G
+        fake = NetG(noise)
+        label.fill_(fake_label)
+        #Classify all fake batch with D
+        output = NetD(fake.detach()).view(-1)
+        #Calculate D's loss on the all fake batch
+        errD_fake = criterion(output, label)
+        errD_fake.backward()
+        D_G_z1 = output.mean().item()
+        
+        #Compute error of D as sum over the fake and real batches
+        errD = errD_real + errD_fake
+        #Update D
+        optimizerD.step()
+        
+        #Update G
+        NetG.zero_grad()
+        label.fill_(real_label)
+        output = NetD(fake).view(-1)
+        #Calculate G loss
+        errG = criterion(output, label)
+        #Calculate gradients for G
+        errG.backward()
+        D_G_z2 = output.mean().item()
+        #Update G
+        optimizerG.step()
+        
+        #Save losses
+        G_losses.append(errG.item())
+        D_losses.append(errD.item())
+        
+        if (iters % 100 == 0) or ((epoch == num_epochs -1) and i == len(dataloader-1)):
+                        with torch.no_grad():
+                            fake = NetG(fixed_noise).detach().cpu()
+                        feature_list.append(vutils.make_grid(fake, padding = 2, normalize = True))
+        
+        iters += 1
