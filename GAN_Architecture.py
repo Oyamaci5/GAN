e62961ac17d9e5ebcee60ec4ab44b6ea024b428b
Oğuzhan Yamacı <77755781+Oyamaci5@users.noreply.github.com>
2021-12-05 18:03:19 +0300

    Update GAN_Architecture.py

--- GAN_Architecture.py
+++ GAN_Architecture.py
@@ -3,50 +3,48 @@ import torch
 import torch.nn as nn
 
 
-ngf = 64
-ndf = 64
+ngf = 32
+ndf = 32
 nz = 100
 
 class Generator(nn.Module):
     def __init__(self, ngpu):
         super(Generator, self).__init__()
         self.ngpu = ngpu
-        
-        self.conv1 = nn.ConvTranspose3d(nz, ngf*16, 4, 1, 0, bias= False )
-        
-        self.main_conv=nn.Sequential(
-            
-            nn.ConvTranspose3d(ngf*16, ngf*8, 4, 2, 1, bias = False),
-            nn.BatchNorm3d(ngf*8),
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose3d( nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm3d(ngf * 8),
             nn.ReLU(True),
-            
-            nn.ConvTranspose3d(ngf*8, ngf*4, 4, 2, 1, bias = False),
-            nn.BatchNorm3d(ngf*4),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose3d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm3d(ngf * 4),
             nn.ReLU(True),
-            
-            nn.ConvTranspose3d(ngf*4, ngf*2, 4, 2, 1, bias = False),
-            nn.BatchNorm3d(ngf*2),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose3d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm3d(ngf * 2),
             nn.ReLU(True),
-             
-            nn.ConvTranspose3d(ngf*2, ngf, 4, 2, 1, bias = False),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose3d( ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm3d(ngf),
             nn.ReLU(True),
-            
-            nn.ConvTranspose3d(ngf, 1, 3, 1, 2, bias=False),
-            nn.ReLU()
-            
-            )
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose3d( ngf, 3, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        )
+
     def forward(self, input):
-        return self.main_conv(input)
-    
+        return self.main(input)
+
 
 class Discriminator(nn.Module):
     def __init__(self,ngpu):
         super(Discriminator, self).__init__()
         self.ngpu = ngpu
         
-        self.main_conv = nn.Sequential(
-                nn.Conv3d(1, ndf, 3, 2, 3, bias = False),
+        self.main = nn.Sequential(
+                nn.Conv3d(3, ndf, 4, 2, 1, bias = False),
                 nn.LeakyReLU(0.2, inplace=True),
                 
                 nn.Conv3d(ndf, ndf*2, 3, 2, 1, bias= False),
@@ -58,15 +56,18 @@ class Discriminator(nn.Module):
                 nn.LeakyReLU(0.2, inplace=True),
                 
                 nn.Conv3d(ndf*4, ndf*8, 3, 2, 1, bias= False),
-                nn.BatchNorm3d(ndf*8),
-                nn.LeakyReLU(0.2, inplace=True),
+                nn.BatchNorm3d(ndf *8 ),
+                nn.LeakyReLU(0.2, inplace = True),
                 
-                nn.Conv3d(ndf*8, 1, 3, 2, 1, bias= False)
+                nn.Conv3d(ndf*8, 1, 3, 1, 0, bias = False),
                 nn.Sigmoid()
+              
                 
             )
+       
+                
     def forward(self, input):
-        return self.main_conv(input)
+         return self.main(input)
 
 def weights_init(m):
     classname = m.__class__.__name__
