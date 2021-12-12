5785be7db58cf72ddd9c9e773bd07f1efe737497
Oğuzhan Yamacı <77755781+Oyamaci5@users.noreply.github.com>
2021-12-12 19:12:52 +0300

    Update GAN_Architecture.py

--- GAN_Architecture.py
+++ GAN_Architecture.py
@@ -11,16 +11,13 @@ class Generator(nn.Module):
     def __init__(self, ngpu):
         super(Generator, self).__init__()
         self.ngpu = ngpu
+        self.conv1 = nn.ConvTranspose3d( nz, ngf * 8, 4, 1, 1, bias=False)
         self.main = nn.Sequential(
-            # input is Z, going into a convolution
-            nn.ConvTranspose3d( nz, ngf * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm3d(ngf * 8),
-            nn.ReLU(True),
-           
+            
             nn.ConvTranspose3d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm3d(ngf * 4),
             nn.ReLU(True),
-           
+       
             nn.ConvTranspose3d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm3d(ngf * 2),
             nn.ReLU(True),
@@ -29,14 +26,17 @@ class Generator(nn.Module):
             nn.BatchNorm3d(ngf),
             nn.ReLU(True),
            
-            nn.ConvTranspose3d( ngf, 3, 4, 2, 1, bias=False),
-            nn.Tanh()
-           
+            nn.ConvTranspose3d( ngf, 1, 4, 1, 1, bias=False),
+            nn.ReLU()
+         
         )
+    
 
-    def forward(self, input):
+    def forward(self, noise, energy):
+        input = self.conv1(noise * energy)
         return self.main(input)
 
+    
 
 class Discriminator(nn.Module):
     def __init__(self,ngpu):
@@ -44,7 +44,7 @@ class Discriminator(nn.Module):
         self.ngpu = ngpu
         
         self.main = nn.Sequential(
-                nn.Conv3d(3, ndf, 3, 2, 1, bias = False),
+                nn.Conv3d(1, ndf, 3 , 2, 1, bias = False),
                 nn.LeakyReLU(0.2, inplace=True),
                 
                 nn.Conv3d(ndf, ndf*2, 3, 2, 1, bias= False),
@@ -59,15 +59,26 @@ class Discriminator(nn.Module):
                 nn.BatchNorm3d(ndf *8 ),
                 nn.LeakyReLU(0.2, inplace = True),
                 
-                nn.Conv3d(ndf*8, 1, 3, 1, 0, bias = False),
+                nn.Conv3d(ndf*8, ndf*4, 3, 2, 1, bias = False),
                 nn.Sigmoid()
               
                 
             )
+        self.fc = nn.Sequential(
+
+            nn.Linear(ndf*4 + 1, ndf*2),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Linear(ndf*2, 1),
+            nn.Sigmoid()
+        )
+
        
-                
-    def forward(self, input):
-         return self.main(input)
+              
+    def forward(self, energy):
+        conv_out = self.main(energy)
+        fc_input = conv_out.view(-1, ndf*4 + 1)
+        return self.fc(fc_input)
 
 def weights_init(m):
     classname = m.__class__.__name__
@@ -76,6 +87,7 @@ def weights_init(m):
     elif classname.find('BatchNorm') != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
-    elif classname.find('Linear')!= -1:
+    elif classname.find('Linear') != -1:
         m.weight.data.normal_(0.0, 0.02)
         m.bias.data.fill_(0)
+    
